5525583e7d28101523a7faa978f9d6d4ffc11e6f
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-02 14:44:19 -0600

    New file push

---
+++ Algorithms/Searching/BinarySearch.js
@@ -0,0 +1,8 @@
+/**
+Problem: Given a sorted array arr[] of n elements, write a function to search a given element x in arr[] and return the index of x in the array. Consider array is 0 base index.
+ */
+
+/**
+used in a sorted array by repeatedly dividing the search interval in half. The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(logn)
+ */
+
