ec420327b3826b6a999b42abd11196b872cb3fcd
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-03 09:26:09 -0600

    Reviewing some clean code practices.

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -15,3 +15,22 @@ Pick median as pivot
 The key process in quickSort is a partition(). The traget of partitions is, given an array and an element x of an array as the pivot, put x at its correct position in a sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) afterx. All this should be done in linear time.
 
  */
+
+function emailClients(clients){
+    clients.forEach(client => {
+    const clientRecord = database.lookup(client);
+    if(clientRecord.isActive()) {
+
+        email(client);
+    }        
+    });
+}
+
+function emailActiveClients(clients){
+clients.filter(isActiveClient).forEach(email)
+}
+
+function isActiveClient(client) {
+    const clientRecord = database.lookup(client);
+return clientRecord.isActive();
+}
