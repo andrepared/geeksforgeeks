065645b1a2f4fc88cdd131eb6cd6e9e3e054ffe1
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-03 13:08:32 -0600

    removing dead code.

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -24,42 +24,7 @@ This seems like an impossible task. UPon first hearing this
 */
 
 /**
-Avoid Type-checking
-
-JS is untyped, meaning your functions can take any type of argument. Sometimes you are bitten bu this freedom and it becomes temptomg to do type-checking in your functions. There are many ways to avoid having to do this. The first thing to consider is consistent APIs.
+Remove dead code.
+Dead code is just as bad as duplicate code. There's no reason to keep it in your codebase. If it's not being called, then get rid of it! It will still be safe in your version history if you still need it.
  */
 
-function travelToTexas(vehicle){
-    if ( vehicle instanceof Bicycle) {
-    vehicle.pedal(this.currentLocation, new Location("texas"));
-} else if (vehicle instanceof Car) {
-    vehicle.drive(this.currentLocation, new Location("texas"));
-
-    }
-}
-
-function travelToTexas(vehicle) {
-vehicle.move(this.currentLocation, new Location("texas"));
-}
-/**
-Avoiding type-checking pt 2. 
-
-If you are working with primitive values like strings and integers, and you can't use polymorphism but you still feel the need to type check, you should consider using TypeScript. It is an excellent alternative to normal JS, as it provides you with statuc typing on top of standard JS syntax. The problem with manually type-checking normal JS is that doing it well requires so much extra verbaige that the faux "type-safety" you get doesn't make up for the lost readability. Keep your JS clean, write good tests, and have good code reviews. Otherwise, do all that but with TypeScript.
- */
-
-function combine(val1, val2) {
-  if (
-    (typeof val1 === "number" && typeof val2 === "number") ||
-    (typeof val1 === "string" && typeof val2 === "string")
-  ) {
-    return val1 + val2;
-  }
-
-  throw new Error("Must be of type String or Number");
-}
-
-// Good 
-
-function combine(val1, val2) {
-    return val1 + val2;
-}
