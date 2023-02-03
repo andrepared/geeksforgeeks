97b5bb6b430af7fca71a36928d8a14970c9f8b68
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-02 21:03:12 -0600

    Ternary summary

--- Algorithms/Searching/TernarySearch.js
+++ Algorithms/Searching/TernarySearch.js
@@ -75,3 +75,44 @@ p = ternarySearch(l, r, key, arr);
 
 // Print the result
 document.write("Index of " + key + " is " + p);
+
+// Time Complexity O (log(3) n)
+// Memory Complexity  O (log(3) n)
+
+/**
+Advantages :
+
+1. Has a time complexity of log base 3, which is more efficient than linear search and comparable to binary search. 
+
+2. Number of comparison get reduced.
+
+3. Works well for large datasets.
+
+4. Fits well with optimization problems. 
+
+5.Ternary Search is a non-recursive algorithm, so it does not require additional memory to store function call stack, this its space efficient.
+
+When To Use:
+
+1. When you have a large ordered array or list and need to find the postion of a specific value.
+
+2. When you need to find the maximum or minimum value of a function.
+
+3. When you need an alternative algorithm for bineary search with an efficient time complexity. 
+
+4. When are you interested in reducing the number of comparisons.
+
+Summary: 
+
+1. Ternary Search is a divide-and conquer algorithm that is used to find the position of a specific value in a given array or list. 
+
+2. It works by dividing the array into three parts and recursively performing the search operation on the appropriate part until the seired element is found. 
+
+3. The algorithm has a time complexity of O(log3n) and is more efficient than a linear search, but less commonly used than other search algorithms like binary search. 
+
+4. It's important to not that the array to be searched must be sorted for Ternary Search to work correctly. 
+
+
+
+
+ */
