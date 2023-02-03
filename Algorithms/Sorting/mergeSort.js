304489b1018883fed4c80cda4dc16caa0ca46651
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-03 13:35:33 -0600

    make objects have private members

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -29,48 +29,22 @@ Dead code is just as bad as duplicate code. There's no reason to keep it in your
  */
 
 /**
-Use getters and setters
-
-using getters and setters to access data could be better than simply looking for a property on an object.
-    1. When you want to do more beyond getting an object propertym you don't have to look up and change every accessor in your codebase. 
-    2. Makes adding validation simple when doing a set/ 
-    3. Encapsulates the internal representation/ 
-    4. Easy to add logging and error handling when getting and setting 
-    5. You can lazy load your object's properties, let's say getting it from a server.
-
-
+Make objects have private members 
+This can be done with closures.
  */
 
-function makeBankAccount() {
-// ....
-    return {
-        balance: 0
-//....    
-    };
-}
-const account = makeBankAccount();
-account.balance = 100;
-
-function makeBankAccount()
-{
-    // this one is private
-    let balance = 0;
-    // a getter made public via the returned object below.
-    function getBalance() 
-    {
-        return balance;
-    }
-    // a setter made public via the returned object below. 
-    function setBalance()
-    {
-        //... validate before updating the balance.
-        balance = amount;    
-    }
-    return {
-    getBalance,
-    setBalance
+const Employee = function (name) {
+    this.name = name;
+};
+Employee.prototype.getName = function getName () {
+    return this.name;
 };
 
-}
-const amount = makeBankAccount();
-account.setBalance();
+const employee = new Employee("John Doe");
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe.
+delete employee.name;
+console.log(`Employee name: ${employee.getName()}`); // Employee name undefined.
+
+
+// Good
+
