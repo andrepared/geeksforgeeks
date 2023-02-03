ea9edba3aa1f3b3fa2b7d8003429f796bb190300
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-03 13:24:52 -0600

    using getters and setter

--- Algorithms/Sorting/mergeSort.js
+++ Algorithms/Sorting/mergeSort.js
@@ -28,3 +28,49 @@ Remove dead code.
 Dead code is just as bad as duplicate code. There's no reason to keep it in your codebase. If it's not being called, then get rid of it! It will still be safe in your version history if you still need it.
  */
 
+/**
+Use getters and setters
+
+using getters and setters to access data could be better than simply looking for a property on an object.
+    1. When you want to do more beyond getting an object propertym you don't have to look up and change every accessor in your codebase. 
+    2. Makes adding validation simple when doing a set/ 
+    3. Encapsulates the internal representation/ 
+    4. Easy to add logging and error handling when getting and setting 
+    5. You can lazy load your object's properties, let's say getting it from a server.
+
+
+ */
+
+function makeBankAccount() {
+// ....
+    return {
+        balance: 0
+//....    
+    };
+}
+const account = makeBankAccount();
+account.balance = 100;
+
+function makeBankAccount()
+{
+    // this one is private
+    let balance = 0;
+    // a getter made public via the returned object below.
+    function getBalance() 
+    {
+        return balance;
+    }
+    // a setter made public via the returned object below. 
+    function setBalance()
+    {
+        //... validate before updating the balance.
+        balance = amount;    
+    }
+    return {
+    getBalance,
+    setBalance
+};
+
+}
+const amount = makeBankAccount();
+account.setBalance();
